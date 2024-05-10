9ee16d4b6976b21160fe7d08230cb0c653c6e46d
LAPTOP-TAFI79L4\Lenovo <zwan0348@student.monash.edu>
2024-05-10 22:09:07 +0800

    header-html

--- 2024/src/App.jsx
+++ 2024/src/App.jsx
@@ -1,6 +1,16 @@
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Header from './components/header/Header'
+
 function App() {
   return (
-    <div className="app">Hello World</div>
+    <div className="app">
+      <Header/>
+      <Router>
+        <Routes>
+          <Route/>
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
---
+++ 2024/src/components/header/Header.jsx
@@ -0,0 +1,46 @@
+import Switch from 'react-switch'
+import './header.css'
+
+
+const Header = () => {
+  return (
+    <div className="nav">
+      <div className="nav_wrapper container">
+        <div className="nav_logo">Rick</div>
+        <ul className="nav_menu">
+          <li className="nav_menu_items">
+            <a href="#home">Home</a>
+          </li>
+          <li className="nav_menu_items">
+            <a href="#services">Services</a>
+          </li>
+          <li className="nav_menu_items">
+            <a href="#skills">Skills</a>
+          </li>
+          <li className="nav_menu_items">
+            <a href="#protfolio">Portfolio</a>
+          </li>
+          <li className="nav_menu_items">
+            <a href="#contact">Contact</a>
+          </li>
+          <div className="buttons">
+            <label htmlFor="" className='switch'>
+              <Switch height={24} width={48} onColor='#4D4D4D' offColor='#ccc'/>
+            </label>
+            <a href="" className='button'>
+              Hire Me
+            </a>
+          </div>
+        </ul>
+      </div>
+      <div className="hamburger-menu">
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Header
